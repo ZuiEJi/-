9de79804776f8caf513f79852fa965d541bd764a
ZuiEJi <103485887+ZuiEJi@users.noreply.github.com>
2022-05-10 14:21:52 +0800

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            add(ref n);
+            System.Console.WriteLine(n);
+        }
+
+        static void add(ref int n)
+        {
+            n = n + 1;
+
+        }
+
+        
+    }
+}
